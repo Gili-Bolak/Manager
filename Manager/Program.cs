556f8c2dbb7efabf0e30ec66e09c197c6c344710
214662124 <214662124@PC1-12>
2024-06-06 14:45:01 +0300

    Add project files.

---
+++ Manager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=SRV2\\PUPILS;Initial Catalog=Shop_214662124;Integrated Security=True;Encrypt=False";
+
+            DataAccess dataAccess = new DataAccess();
+
+            string toContinue = "y";
+            while(toContinue == "y")
+            {
+                dataAccess.InsertCategory(connectionString);
+                Console.WriteLine("Would you like to continue? y/n");
+                toContinue = Console.ReadLine();
+            }
+
+            dataAccess.ReadCategories(connectionString);
+            
+        }
+    }
+}
